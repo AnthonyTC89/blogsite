808bba72c65c01d0dcec1170e23f3a764c3e938f
AnthonyTC89 <ptonyptc@gmail.com>
2020-07-03 21:12:52 -0500

    put route to posts initialized

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -22,4 +22,18 @@ router.post('/api/posts', async (req, res) => {
   res.json({ status: 'OK', post });
 });
 
+router.put('/api/posts/:id', async (req, res) => {
+  const { title, text } = req.body;
+  const post = new Post(
+    {
+      title,
+      text,
+      status: true,
+      created_at: Date.now(),
+    },
+  );
+  await post.save();
+  res.json({ status: 'OK', post });
+});
+
 module.exports = router;
