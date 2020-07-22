fedd677ecb3a2789d8fc51a13227de90f7b483da
AnthonyTC89 <ptonyptc@gmail.com>
2020-07-22 18:02:44 -0500

    img phone added to home

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Navbar from '../Components/Navbar';
+import React, { useState } from 'react';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import srcPhone from '../Images/phone.png';
+import './Home.css';
 
-const Home = ({ history }) => (
-  <>
-    <Navbar history={history} />
-  </>
-);
+const Home = () => {
+  const [showSignIn, setShowSignIn] = useState(true);
 
-Home.propTypes = {
-  history: PropTypes.object.isRequired,
+  const handleComponent = () => {
+    setShowSignIn(!showSignIn);
+  };
+
+  // const srcImg = 'https://cdn.pixabay.com/photo/2015/09/17/14/24/guitar-944262_960_720.jpg';
+
+  return (
+    <div className="container">
+      <div className="row home-row">
+        <picture className="col-md-6 home-column home-picture">
+          <img src={srcPhone} alt="home-img" className="home-phone" />
+        </picture>
+        <div className="col-md-6 home-column">
+          {showSignIn ? <SignIn handleComponent={handleComponent} />
+            : <SignUp handleComponent={handleComponent} />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/database.js
+++ src/database.js
@@ -6,6 +6,8 @@ const USERNAME = process.env.USERNAME_MONGODB;
 const PASSWORD = process.env.PASSWORD_MONGODB;
 
 const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@portfolio-atc-n69h1.mongodb.net/blogsite?retryWrites=true&w=majority`;
+
+mongoose.set('useCreateIndex', true);
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Successful connection MongoDB Atlas');
