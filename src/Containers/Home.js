932be227c20e8b233ff53836d301529a7820a8f3
AnthonyTC89 <ptonyptc@gmail.com>
2020-08-03 14:05:44 -0500

    Footer added

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import SignIn from '../Components/SignIn';
 import SignUp from '../Components/SignUp';
+import Footer from '../Components/Footer';
 import srcPhone from '../Images/phone.png';
 import { InfoHome } from '../Info.json';
 import './Home.css';
@@ -15,18 +16,21 @@ const Home = ({ history }) => {
   };
 
   return (
-    <div className="container">
-      <div className="row home-row">
-        <picture className="d-none d-sm-flex col-sm-6 home-picture">
-          <img src={srcPhone} alt="home-phone" className="home-phone" />
-          <img src={Images[0]} alt="home-img" className="home-img" />
-        </picture>
-        <div className="col-12 col-sm-6 justify-content-center justify-content-center justify-content-md-start home-component">
-          {showSignIn ? <SignIn history={history} handleComponent={handleComponent} />
-            : <SignUp history={history} handleComponent={handleComponent} />}
+    <>
+      <div className="container">
+        <div className="row home-row">
+          <picture className="d-none d-sm-flex col-sm-6 home-picture">
+            <img src={srcPhone} alt="home-phone" className="home-phone" />
+            <img src={Images[0]} alt="home-img" className="home-img" />
+          </picture>
+          <div className="col-12 col-sm-6 justify-content-center justify-content-center justify-content-md-start home-component">
+            {showSignIn ? <SignIn history={history} handleComponent={handleComponent} />
+              : <SignUp history={history} handleComponent={handleComponent} />}
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
